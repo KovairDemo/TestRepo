8c2c71d22efb3b06a0343e48c930f56ccf7698cd
ab187 <ab187@demo1.sr.kovair.com>
2017-08-30 03:18:09 -0700

    KDP-1145

--- src/demoPackage/Main.java
+++ src/demoPackage/Main.java
@@ -3,5 +3,6 @@ package demoPackage;
 public class Main {
 	public static void main(String []args) {
 		System.out.println("Hello World"); // Comment added for KDP-1143
+		System.out.println("Success Point");
 	}
 }
