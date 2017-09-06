55a01a64404710912390b07d5023197af3e817e4
ab187 <ab187@demo1.sr.kovair.com>
2017-09-06 02:00:31 -0700

    448

--- src/demoPackage/Main.java
+++ src/demoPackage/Main.java
@@ -3,6 +3,6 @@ package demoPackage;
 public class Main {
 	public static void main(String []args) {
 		System.out.println("Hello World"); // Comment added during Test14
-		System.out.println("Success Point");
+		System.out.println("Success Point.");
 	}
 }
