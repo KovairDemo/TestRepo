8e07d5ab7ee961832272d2e1ef784cc70d7eb838
vg262 <vg262@demo1.sr.kovair.com>
2018-01-10 04:08:37 -0800

    581;Implemented

--- SignupPageProject/src/Signup/UserInfo.java
+++ SignupPageProject/src/Signup/UserInfo.java
@@ -1,10 +1,35 @@
 package Signup;
 
-public class UserInfo {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.print("Finger print sensor defect 582");
 
-	}
 
-}
+
+		class GetInputData
+		{
+		  public static void main(String args[])
+		  {
+		     int num;
+		     double fnum;
+		     String str;
+		 
+		     Scanner in = new Scanner(System.in);
+		 
+		     //Get input String
+		     System.out.println("Enter a string: ");
+		     str = in.nextLine();
+		     System.out.println("Input String is: "+str);
+		 
+		     //Get input Integer
+		     System.out.println("Enter an integer: ");
+		     num = in.nextInt();
+		     System.out.println("Input Integer is: "+num);
+		 
+		     //Get input float number
+		     System.out.println("Enter a float number: ");
+		     fnum = in.nextFloat();
+		     System.out.println("Input Float number is: "+fnum); 
+		  }
+		}
+	
+
