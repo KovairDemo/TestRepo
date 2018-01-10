89955da9228fac602e722655257cc802337ab9f4
vg262 <vg262@demo1.sr.kovair.com>
2018-01-09 22:07:46 -0800

    574;Implemented

--- SignupPageProject/src/Signup/UserInfo.java
+++ SignupPageProject/src/Signup/UserInfo.java
@@ -3,7 +3,7 @@ package Signup;
 public class UserInfo {
 
 	public static void main(String[] args) {
-		System.out.print("Finger print sensor defect");
+		System.out.print("Finger print sensor defect 574");
 
 	}
 
