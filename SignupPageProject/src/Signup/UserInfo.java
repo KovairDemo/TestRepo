1d5c6664088546203dd1dc24517a680be52f9f25
vg262 <vg262@demo1.sr.kovair.com>
2018-01-23 05:32:38 -0800

    dd

--- SignupPageProject/src/Signup/UserInfo.java
+++ SignupPageProject/src/Signup/UserInfo.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 		     Scanner in = new Scanner(System.in);
 		 
 		     //Get input String
-		     System.out.println("Enter a string1: ");
+		     System.out.println("Enter a string: ");
 		     str = in.nextLine();
 		     System.out.println("Input String is: "+str);
 		 
